d76101ca69f2be3fd71b901465b7efa6cf067728
Egor Shitikov <egorshitikov@gmail.com>
2021-09-24 17:52:22 -0700

    Update install-terraform.sh

--- install-terraform.sh
+++ install-terraform.sh
@@ -3,6 +3,7 @@ export TERRAFORM_VERSION=1.0.7 \
 && export TERRAGRUNT_VERSION=0.31.11 \
 && mkdir -p /ci/terraform_${TERRAFORM_VERSION} \
 && wget -nv -O /ci/terraform_${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip /ci/terraform_${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /ci/terraform_${TERRAFORM_VERSION}/ \
 && mkdir -p /ci/terragrunt-${TERRAGRUNT_VERSION}/ \
 && wget -nv -O /ci/terragrunt-${TERRAGRUNT_VERSION}/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 \
 && sudo chmod a+x /ci/terragrunt-${TERRAGRUNT_VERSION}/terragrunt \
