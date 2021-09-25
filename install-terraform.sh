f85bb5e4e9bc204d03ddfd2d21d13d755533cf50
Egor Shitikov <egorshitikov@gmail.com>
2021-09-25 15:54:51 -0700

    Add terraform installation script

--- install-terraform.sh
+++ install-terraform.sh
@@ -1,13 +1,15 @@
-sudo -s; ## run as a super user
+## run as a super user
 export TERRAFORM_VERSION=1.0.7 \
 && export TERRAGRUNT_VERSION=0.31.11 \
 && mkdir -p /ci/terraform_${TERRAFORM_VERSION} \
 && wget -nv -O /ci/terraform_${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
 && unzip /ci/terraform_${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /ci/terraform_${TERRAFORM_VERSION}/ \
-&& mkdir -p /ci/terragrunt-${TERRAGRUNT_VERSION}/ \
-&& wget -nv -O /ci/terragrunt-${TERRAGRUNT_VERSION}/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 \
-&& sudo chmod a+x /ci/terragrunt-${TERRAGRUNT_VERSION}/terragrunt \
-&& cp /ci/terragrunt-${TERRAGRUNT_VERSION}/terragrunt /bin \
+&& sudo chmod a+x /ci/terraform_${TERRAFORM_VERSION}/terraform \
+&& cp /ci/terraform_${TERRAFORM_VERSION}/terraform /usr/local/bin/ \
+&& chmod a+x /bin/terraform \
+&& mkdir -p /ci/terragrunt_${TERRAGRUNT_VERSION}/ \
+&& wget -nv -O /ci/terragrunt_${TERRAGRUNT_VERSION}/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 \
+&& sudo chmod a+x /ci/terragrunt_${TERRAGRUNT_VERSION}/terragrunt \
+&& cp /ci/terragrunt_${TERRAGRUNT_VERSION}/terragrunt /usr/local/bin/ \
 && chmod a+x /bin/terragrunt \
-&& rm -rf /ci \
-&& exit
+&& rm -rf /ci 
